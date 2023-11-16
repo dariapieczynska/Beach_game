5c780c2ae1b490eeada5f7fa59eae68d3bd7498d
dariapieczynska <148916267+dariapieczynska@users.noreply.github.com>
2023-11-16 14:50:12 -0800

    weird moving hehe

---
+++ Beach game/Assets/Daria's folder/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Pieczynska, Daria
+// 11/16/2023
+//Player moves forwared, backward and to the sides
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public bool canTurn = true; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            canTurn = true;
+            TurnRight();
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            canTurn = true;
+            TurnLeft();
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+
+
+    }
+    public void TurnRight()
+    {
+        if (canTurn)
+        {
+
+
+            if (Input.GetKeyDown("d") || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+
+                transform.Rotate(Vector3.up * 90);
+                canTurn = false;
+            }
+        }
+    }
+    public void TurnLeft()
+    {
+        if (canTurn)
+        {
+
+
+            if (Input.GetKeyDown("a") || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+
+                transform.Rotate(Vector3.up * -90);
+                canTurn = false;
+            }
+        }
+    }
+    
+}

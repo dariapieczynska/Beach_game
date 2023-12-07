05f12e020641633a02567632f0727bb4ef43dfe5
cielaparent <148916741+cielaparent@users.noreply.github.com>
2023-12-07 14:13:13 -0800

    idk

--- Beach game/Assets/Ciela's Folder/Scripts/FriendTracker.cs
+++ Beach game/Assets/Ciela's Folder/Scripts/FriendTracker.cs
@@ -12,9 +12,9 @@ public class FriendTracker : MonoBehaviour
 
     //lives start at 0 and go up when you drop of friends
     public int lives = 0;
-    public GameObject friend1;
-    public GameObject friend2;
-    public GameObject friend3;
+    public FriendsCar friend1;
+    public FriendsCar friend2;
+    public FriendsCar friend3;
 
  
     public TimeController Timer;
@@ -29,18 +29,19 @@ public class FriendTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (friend1 != null)
+        if (friend1.droppedOff)
         {
             FriendsText.text = "Friends: 1/3";
         }
-        if (friend2 != null) 
+        if (friend2.droppedOff) 
         {
             FriendsText.text = "Friends: 2/3";
         }
-        if (friend3 != null)
+        if (friend3.droppedOff)
         {
             FriendsText.text = "Friends: 3/3";
         }
+        
 
     }
 

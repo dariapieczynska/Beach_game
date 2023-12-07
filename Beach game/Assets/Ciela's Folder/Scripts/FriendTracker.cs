0f5710746d624d709e84297535d6872eef53f176
dariapieczynska <148916267+dariapieczynska@users.noreply.github.com>
2023-12-07 14:45:06 -0800

    changes

--- Beach game/Assets/Ciela's Folder/Scripts/FriendTracker.cs
+++ Beach game/Assets/Ciela's Folder/Scripts/FriendTracker.cs
@@ -29,15 +29,15 @@ public class FriendTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (friend1.droppedOff)
+        if (friend1 != null)
         {
             FriendsText.text = "Friends: 1/3";
         }
-        if (friend2.droppedOff) 
+        if (friend2 != null) 
         {
             FriendsText.text = "Friends: 2/3";
         }
-        if (friend3.droppedOff)
+        if (friend3 != null)
         {
             FriendsText.text = "Friends: 3/3";
         }
